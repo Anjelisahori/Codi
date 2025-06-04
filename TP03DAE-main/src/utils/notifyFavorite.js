a810c3fde7786e43b84bd4503935b2f47c522350
Anjeli Verastigue <anjeli.verastigue@tecsup.edu.pe>
2025-06-04 17:10:49 -0500

    e

--- TP03DAE-main/src/utils/notifyFavorite.js
+++
@@ -1,7 +0,0 @@
-import { toast } from 'react-toastify'
-
-export const notifyFavorite = (added) => {
-  toast[added ? 'success' : 'info'](
-    added ? 'Agregado a favoritos' : 'Removido de favoritos'
-  )
-}
